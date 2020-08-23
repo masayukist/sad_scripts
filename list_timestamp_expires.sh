78deb61e2d28439e728e8ba0ebb92443a07183be
root <root@ciero.gp.sc.cc.tohoku.ac.jp>
2020-08-23 20:57:18 +0900

    add scripts to know expirations of accounts

---
+++ list_timestamp_expires.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+source ./include/description.sh
+
+list_user_enabled () {
+	USER_LIST=( `samba-tool user list` )
+	for x in ${USER_LIST[@]}
+	do
+		USER_DESC=`description ${x}`
+		EXPIRE_TIMESTAMP=`./include/user_accountexpires.sh ${x}`
+		if [ "`./is_user_disabled.sh ${x}`" = "ENABLED" ]
+		then
+			echo ${EXPIRE_TIMESTAMP} ${x} \(${USER_DESC}\)
+		fi
+	done
+}
+
+list_user_enabled
